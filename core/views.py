900465d90e8aa3b1abe7736ef4870bf32fb088ab
developerNihad <nihadnahmatli@gmail.com>
2025-04-03 16:28:06 +0400

    first commit

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def homepage(request): 
+    return HttpResponse("Hello World! I'm home.")
+
+
+def about(request):
+    return HttpResponse("My About page.")
