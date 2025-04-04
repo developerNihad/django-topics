846b48dfbed25f7bef1de886b10fa2312e7510f2
developerNihad <nihadnahmatli@gmail.com>
2025-04-04 22:02:40 +0400

    commited

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 def homepage(request): 
-    return HttpResponse("Hello World! I'm home.")
+    # return HttpResponse("Hello World! I'm home.")
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse("My About page.")
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')
